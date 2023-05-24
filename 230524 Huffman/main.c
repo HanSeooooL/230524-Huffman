d5119bbbd0097243adca679271a3715f30873e32
HANSEOL <nothinonthere@icoud.com>
2023-05-24 12:31:03 +0900

    Initial Commit

---
+++ 230524 Huffman/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  230524 Huffman
+//
+//  Created by 한설 on 2023/05/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
