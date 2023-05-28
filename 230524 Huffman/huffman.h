4a140bcbac0dc17573939dab3c8fb34cbeeb25be
HANSEOL <nothinonthere@icoud.com>
2023-05-28 23:17:14 +0900

    [update] 제출

---
+++ 230524 Huffman/huffman.h
@@ -0,0 +1,17 @@
+//
+//  huffman.h
+//  230524 Huffman
+//
+//  Created by 한설 on 2023/05/28.
+//
+
+#ifndef huffman_h
+#define huffman_h
+
+#include <stdio.h>
+
+char *huffmanencode(char *name);
+
+void filesave(char *str);     //파일 저장
+
+#endif /* huffman_h */
